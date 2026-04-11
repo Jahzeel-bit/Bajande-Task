754cccf4fae474f917c42fc84f92c1403f5b87f5
Jahzeel-bit <mkjhzlbjnd@gmail.com>
2026-04-11 12:34:04 +0000

    update number of days challenge

--- challenge/NumberOfDays.java
+++ challenge/NumberOfDays.java
@@ -3,61 +3,78 @@ import java.time.*;
 import java.util.Scanner;
 
 public class NumberOfDays {
-    
-    public void daysOfMonth(LocalDate m, int d3){
-        LocalDate m1 = m.plusMonths(d3);
-        int day = m1.lengthOfMonth();
-    }
-    void monthName(LocalDate name){
+    int day;
+    String month;
+    NumberOfDays(LocalDate m, int n){
+        LocalDate numMonth = m.plusMonths(n);
+        day = numMonth.lengthOfMonth();
+        month = numMonth.getMonth().name();
         
     }
+    void result(){
+        System.out.println(month+" has "+day +" days");
+    }
     public static void main(String[] args) { 
-        NumberOfDays dm = new NumberOfDays();    
+           
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter Month: ");
         int d1 = scan.nextInt();
-
-        LocalDate d = LocalDate.of(2026, 1, 1);      
+        
+        LocalDate d = LocalDate.of(2026, 1, 1);
+        NumberOfDays dm = new NumberOfDays(d, d1-1);       
         if (d1<=12){
             switch (d1) {
                 case 1:
-                    int numDays = d.lengthOfMonth();
-                    System.out.println("January has "+numDays +"days");
+                    dm.result();
+                    // int numDays = d.lengthOfMonth();
+                    // System.out.println("January has "+numDays +"days");
                     break;
                 case 2:
+                    dm.result();
                     // LocalDate numMonth = d.plusMonths(1);
                     // String month = numMonth.getMonth().name();
                     // int day = numMonth.lengthOfMonth();
                     // System.out.println(month+" has "+day +" days");
                     break;
                 case 3:
-                    dm.daysOfMonth(d, 3);
-                    int days = day.daysOfMonth();
-                    System.out.println(month+" has "+ days +" days");
+                    dm.result();
+                    
                     break;
                 case 4:
+                    dm.result();
                     break;
                 case 5:
+                    dm.result();
                     break;
                 case 6:
+                    dm.result();
                     break;
                 case 7:
+                    dm.result();
                     break;
                 case 8:
+                    dm.result();
                     break;
                 case 9:
+                    dm.result();
                     break;
                 case 10:
+                    dm.result();
                     break;
                 case 11:
+                    dm.result();
                     break;
                 case 12:
+                    dm.result();
                     break;
                 default:
+                    System.out.println(d1+" is invalid month");
                     break;
             }
         }
-        
+        else{
+            System.out.println(d1+" is invalid number of month");
+        }
         scan.close();
     }
 }
